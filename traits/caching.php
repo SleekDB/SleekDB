ad4839e484c93b921a1c1f487068d2141bb37932
rakibtg <rakibtg@gmail.com>
2018-04-17 22:16:29 +0600

    Caching API working!

--- traits/caching.php
+++ traits/caching.php
@@ -5,8 +5,58 @@
    */
   trait CacheTraits {
     
-    private function makeCache() {
-      
+    // Make cache deletes the old cache if exists then creates a new cache file.
+    // returns the data.
+    private function reGenerateCache() {
+      $token  = $this->getCacheToken();
+      $result = $this->findStore();
+      // Write the cache file.
+      file_put_contents( $this->getCachePath( $token ), json_encode( $result ) );
+      // Reset cache flags to avoid future queries on the same object of the store.
+      $this->resetCacheFlags();
+      // Return the data.
+      return $result;
+    }
+
+    // Use cache will first check if the cache exists, then re-use it.
+    // If cache dosent exists then call makeCache and return the data.
+    private function useExistingCache() {
+      $token = $this->getCacheToken();
+      // Check if cache file exists.
+      if ( file_exists( $this->getCachePath( $token ) ) ) {
+        // Reset cache flags to avoid future queries on the same object of the store.
+        $this->resetCacheFlags();
+        // Return data from the found cache file.
+        return json_decode( file_get_contents( $this->getCachePath( $token ) ), true );
+      } else {
+        // Cache file was not found, re-generate the cache and return the data.
+        return $this->reGenerateCache();
+      }
+    }
+
+    // This method would make a unique token for the current query.
+    // We would use this hash token as the id/name of the cache file.
+    private function getCacheToken() {
+      $query = json_encode( [
+        'store' => $this->storeName,
+        'limit' => $this->limit,
+        'skip' => $this->skip,
+        'condition' => $this->conditions,
+        'order' => $this->orderBy,
+        'search' => $this->searchKeyword
+      ] );
+      return md5($query);
+    }
+
+    // Reset the cache flags so the next database query dosent messedup.
+    private function resetCacheFlags() {
+      $this->makeCache = false;
+      $this->useCache  = false;
+    }
+
+    // Returns the cache directory absolute path for the current store.
+    private function getCachePath( $token ) {
+      return $this->storeName . '/cache/' . $token . '.json';
     }
 
   }
