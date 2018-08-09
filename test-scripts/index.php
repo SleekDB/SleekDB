11ee334ceb3ebafdc8002d385ef8f3bf87ebe105
rakibtg <rakibtg@gmail.com>
2018-08-09 22:10:36 +0600

    test script

--- test-scripts/index.php
+++ test-scripts/index.php
@@ -3,17 +3,19 @@
   require_once '../src/SleekDB.php';
 
   $sdb = new \SleekDB\SleekDB( "items" );
-  $items = [
-    [
-      "title" => "Google Pixel 2",
-      "about" => "The unlocked Pixel 2 provides..."  
-    ],
-    [
-      "title" => "Google Pixel XL",
-      "about" => "The unlocked biggest Pixel 2..."  
-    ]
-  ];
-  $results = $sdb->insertMany( $items ); 
-  // print_r($results);
+  /*
+    $items = [
+      [
+        "title" => "Google Pixel 2",
+        "about" => "The unlocked Pixel 2 provides..."  
+      ],
+      [
+        "title" => "Google Pixel XL",
+        "about" => "The unlocked biggest Pixel 2..."  
+      ]
+    ];
+    $results = $sdb->insertMany( $items ); 
+    print_r($results);
+  */
   $res = $sdb->fetch();
   print_r($res);
