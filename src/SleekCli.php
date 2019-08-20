c00e4746d0edcdb2dbeb907216d6dcbb67e322d0
aaron <aaronmcquade21@gmail.com>
2019-08-20 00:38:19 -0400

    * added packed CLImate * Created class SleekCLI

---
+++ src/SleekCli.php
@@ -0,0 +1,125 @@
+<?php
+require_once('vendor/autoload.php');
+require_once('SleekDB.php');
+
+class SleekCli extends League\CLImate\CLImate {
+    public $sdb;
+    public $data_dir;
+    public $args;
+
+    function __construct($ini_array = false ) {
+        parent::__construct();
+        if(!$ini_array) $ini_array = $this->create_config();
+        $this->data_dir = $ini_array['data_directory'] ?? 'sleek_db';
+        $this->sdb = new \SleekDB\SleekDB($this->data_dir, $ini_array);
+        
+        // define args 
+        $this->args = [
+            'create-store' => [
+                'prefix'       => 'c',
+                'longPrefix'   => 'create-store',
+                'description'  => 'Create a new store',
+                'defaultValue' => 'me_myself_i',
+            ],
+            'delete-store' => [
+                'prefix'      => 'd',
+                'longPrefix'  => 'delete-store',
+                'description' => 'Delete a store',
+            ],
+            'delete-object' => [
+                'prefix'      => 'do',
+                'longPrefix'  => 'delete-object',
+                'description' => 'Delete a object into a store',
+            ],
+            'insert' => [
+                'prefix'      => 'i',
+                'longPrefix'  => 'insert',
+                'description' => 'Insert a object into a store'
+            ],
+            'fetch' => [
+                'prefix'      => 'f',
+                'longPrefix'  => 'fetch',
+                'description' => 'Fetch all objects from a store'
+            ],
+            'insert-many' => [
+                'prefix'      => 'im',
+                'longPrefix'  => 'insert',
+                'description' => 'Insert an object into a store'
+            ],
+            'data' => [
+                'longPrefix'  => 'data',
+                'description' => 'JSON object '
+            ],
+            'list-stores' => [
+                'prefix'      => 'l',
+                'longPrefix'  => 'list-stores',
+                'description' => 'List all stores in the data directory.',
+                'noValue' => true
+            ],
+            'help' => [
+                'longPrefix'  => 'help',
+                'description' => 'Prints a usage statement',
+            ]
+            ];
+        $this->arguments->add($this->args);
+
+    }
+
+    // create config.ini file
+    public function create_config() {
+        $input = $this->confirm('No config file found. Create one now?');
+        if(!$input->confirmed()) die("Please create a config.ini file.\n");
+
+        $ini_array = [];
+        $input = $this->input('Enter a data directory:');
+        $ini_array['data_directory'] = $input->prompt(); 
+
+        $input = $this->confirm('Enable auto-caching?');
+        $ini_array['auto_cache'] = $input->confirmed(); 
+
+        $input = $this->input('Enter Timeout length (int):');
+        $ini_array['timeout'] = $input->prompt(); 
+
+        foreach($ini_array as $config => $value ){
+            $line = "{$config} = {$value}\n";
+            file_put_contents('config.ini', $line, FILE_APPEND | LOCK_EX);
+        }
+
+        return $ini_array;
+    }
+
+
+    public function check_args() {
+        $this->arguments->parse();
+        foreach($this->args as $arg => $values) {
+            if($this->arguments->get($arg)) {
+                echo "$arg : {$this->arguments->get($arg)} \n";
+                switch($arg){
+                    case 'delete-store':
+                        $this->sdb->store($this->arguments->get($arg), $this->data_dir)->deleteStore();
+                        break;
+                    case 'create-store':
+                        $this->sdb->store($this->arguments->get($arg), $this->data_dir);
+                        break;
+                    case 'fetch':
+                        $store = $this->sdb->store($this->arguments->get($arg), $this->data_dir)->fetch();
+                        print_r('Store: ' . $store);
+                        break;  
+                    case 'insert':
+                        $store = $this->sdb->store($this->arguments->get($arg), $this->data_dir);
+                        $data = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $this->arguments->get('data')), true );
+                        $res = $store->insert($data);
+                        break;
+                    case 'delete-record':
+                        $store = $this->sdb->store($this->arguments->get($arg), $this->data_dir);
+                        $data = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $this->arguments->get('data')), true );
+                        $res = $store->where($data['fieldName'], $data['condition'], $data['value'])->delete();
+                        break;    
+
+                }
+            }
+        }
+    }
+
+}
+?>
--- src/cli.php
+++ src/cli.php
@@ -1,115 +1,12 @@
 #!/usr/bin/env php
 <?php
-function write_ini($data, $location='config.ini'){
-    foreach($data as $d){
-        $string = "{$d['setting']} = {$d['value']}";
-        file_put_contents($location, $string, FILE_APPEND | LOCK_EX);
-    }
-}
-
-function print_usage(){
-    echo 'hi';
-}
 
 if ('cli' !== php_sapi_name()) {
     return;
 } else {
-    require_once('SleekDB.php');
-    ///use SleekDB;
+    require_once('SleekCli.php');
     $ini_array = parse_ini_file("config.ini");
-    if($ini_array === false) {
-      echo "No config.ini found, create one now? (y/n): ";
-      $line = trim(fgets(STDIN)); 
-        if($line == 'y') {
-            $data = [];
-            echo "Enter a data dir: ";
-            $data_dir = trim(fgets(STDIN)); 
-            array_push($data, ['setting'=> 'data_dir', 'value' => $data_dir]); 
-            
-            // write ini
-            write_ini($data, 'config.ini');
-        } 
-    } else {
-        $data_dir = $ini_array['data_dir'];
-        unset($ini_array['data_dir']);
-    }
-    $sdb = new \SleekDB\SleekDB($data_dir);
+    $cli = new SleekCli($ini_array);
+    $cli->check_args();
+    exit;
 }
-
-// var_dump($sdb);
-// exit;
-
-// configure args
-$shortopts  = "";
-$shortopts .= "c:";  // Required value
-$shortopts .= "d:";  // Required value
-
-$shortopts .= "f:";  // Required value
-$shortopts .= "i:";  // Required value
-$shortopts .= "h:";  // Required value
-
-$opts = [
-        'c' => "create:", 
-        'd' => "delete:",
-        'dr' => "delete-record:",
-        'da' => "data:",
-        'f' => "fetch:",    
-        'i' => "insert:",
-        'l' => 'list:' 
-];
-
-$options = getopt(implode(':', array_keys($opts)), array_values($opts));
-
-print_r($options);
-switch($options) {
-    case $options['help'] || $options['h']: 
-        print_usage();
-    break;
-
-    // list
-    case $options['list'] || $options['l']:
-        // find all stores in data dir
-
-        // loop thru and print name
-        foreach ($stores as $s)
-        $store = $sdb->store((!is_null($options['f']) ? $options['f'] : $options['fetch']), $data_dir);
-        $res = $fetch = $store->fetch();
-        print_r($fetch);
-        break;
-
-    // create
-    case $options['create-store'] || $options['c']: 
-        $res = $sdb->store((!is_null($options['c']) ? $options['c'] : $options['create']), $data_dir);
-        break;
-
-    // delete-store
-    case $options['d'] || $options['delete-store']: 
-        $store = $sdb->store((!is_null($options['d']) ? $options['d'] : $options['delete']), $data_dir);
-        $res = $store->deleteStore();
-        break;    
-
-    // insert
-    case ($options['i'] || $options['insert']) && !is_null($options['data']):
-        $store = $sdb->store((!is_null($options['i']) ? $options['i'] : $options['insert']), $data_dir);
-        $data = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $options['data']), true );
-        $res = $store->insert($data);
-        break;
-
-
-    // delete-record
-    case $options['delete-record'] && !is_null($options['data']): 
-        $store = $sdb->store($options['delete-record'], $data_dir);
-        $data = json_decode( preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $options['data']), true );
-        $res = $store->where($data['fieldName'], $data['condition'], $data['value'])->delete();
-        break;
-
-    // fetch
-    case $options['f'] || $options['fetch']: 
-        $store = $sdb->store((!is_null($options['f']) ? $options['f'] : $options['fetch']), $data_dir);
-        $res = $fetch = $store->fetch();
-        print_r('DD: ' . $fetch);
-        break;
-        
-}
-
-print_r($res);
