fa77638a46502337a419c44e736e4dd2f9869bf5
Kazi Mehedi Hasan <rakibtg@gmail.com>
2020-11-21 13:45:50 +0600

    Added getSleekDB and caseRunner helper

--- tests/nest/nest.helpers.php
+++ tests/nest/nest.helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use \SleekDB\SleekDB;
+
 function equalItems($arrayWithoutId, $arrayWithId, $removeId = true)
 {
   $dataMatched = false;
@@ -38,4 +40,41 @@ function idExistsInEveryItem($items)
     }
   }
   return $test;
+}
+
+function getSleekDB($store, $autoCache = false)
+{
+  return SleekDB::store($store, __DIR__ . "/nest-test-db-store/", [
+    'auto_cache' => $autoCache
+  ]);
+}
+
+function caseRunner($cases)
+{
+  $message = '';
+  $result = true;
+  foreach ($cases as $key => $case) {
+    $data = ($case['query'])->fetch();
+
+    // Check rows_count
+    if (count($data) !== $case['rows_count']) {
+      $message = $case['name'] . "\nExpected row count is " . $case['rows_count'] . " but received " . count($data);
+      $result = false;
+      break;
+    }
+
+    // Check validator
+    if (isset($case['validator'])) {
+      $validator = ($case['validator'])($data);
+      if ($validator !== true) {
+        $message = $case['name'] . "\n" . $validator;
+        $result = false;
+        break;
+      }
+    }
+  }
+  return [
+    'message' => $message,
+    'result' => $result
+  ];
 }
