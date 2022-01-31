e434f857fba666bacef6c7b7d5e19e03cb79c248
rakibtg <rakibtg@gmail.com>
2022-01-31 23:48:02 +0100

    Updated default value of timeout to boolean false

--- src/Store.php
+++ src/Store.php
@@ -40,7 +40,7 @@ class Store
   protected $useCache = true;
   protected $defaultCacheLifetime;
   protected $primaryKey = "_id";
-  protected $timeout = 120;
+  protected $timeout = false;
   protected $searchOptions = [
     "minLength" => 2,
     "scoreKey" => "searchScore",
