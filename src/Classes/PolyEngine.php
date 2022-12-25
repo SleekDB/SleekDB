c421eccf8f86ee56527bd063fab5cd3a75ef28c2
Kazi Mehedi Hasan <rakibtg@gmail.com>
2022-12-25 19:51:15 +0000

    init poly engine

---
+++ src/Classes/PolyEngine.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SleekDB\Classes;
+
+use \SleekDB\Classes\Engine;
+
+class PolyEngine {
+    public static function getEngineName() {
+        return Engine::POLY;
+    }
+    
+    // public 
+}
